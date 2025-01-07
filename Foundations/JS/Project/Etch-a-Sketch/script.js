42418c2b41fedcb3a4115ff6345f927e2feddee4
Aaron Huang <aaronhuang0000@gmail.com>
2025-01-07 17:27:18 -0500

    added 'hover' effect - used JS to add an event listener to all divs inside the container that when hovered it adds the class where it's background color is changed

--- Foundations/JS/Project/Etch-a-Sketch/script.js
+++ Foundations/JS/Project/Etch-a-Sketch/script.js
@@ -1,6 +1,14 @@
 
 const container = document.querySelector(".container")
 for(let i=0; i<(16*16); i++){ 
-    let create_row= document.createElement("div");
-    container.appendChild(create_row);
+    let boxes= document.createElement("div");
+    boxes.setAttribute("class", "sketching_divs")
+    container.appendChild(boxes);
 }
+
+const all_boxes = document.querySelectorAll(".sketching_divs")
+all_boxes.forEach((div_box) => {
+    div_box.addEventListener("mouseover", () => {
+        div_box.classList.add("sketch");
+    })    
+});
