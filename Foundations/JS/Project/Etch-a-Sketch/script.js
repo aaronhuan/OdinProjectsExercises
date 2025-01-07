cb5a66bec83f2c51d925e6d95542f0d53767d300
Aaron Huang <aaronhuang0000@gmail.com>
2025-01-07 16:58:42 -0500

    start etch a sketch js project - initialized html, css, js files - created a div called 'container' that would 'contain' the divs - did a loop that created 16 x 16 divs - used flexbox (basis-> empty divs would render) (wrap-> to 'wrap' into another row at the end of container')(aspect-ratio-> to make it a square)

---
+++ Foundations/JS/Project/Etch-a-Sketch/script.js
@@ -0,0 +1,6 @@
+
+const container = document.querySelector(".container")
+for(let i=0; i<(16*16); i++){ 
+    let create_row= document.createElement("div");
+    container.appendChild(create_row);
+}
