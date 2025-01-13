5ab714c799d0cf4bda25e9e2db7c5db7098d7377
Aaron Huang <aaronhuang0000@gmail.com>
2025-01-13 11:17:10 -0500

    improved readability/repetitiveness - made buttons with helper function - used flexbox to centerize the calculator - added color to background of calculator, buttons, and display

--- Foundations/JS/Project/Calculator/script.js
+++ Foundations/JS/Project/Calculator/script.js
@@ -39,44 +39,46 @@ function operate(op, num1, num2){
 
 
 //step 4 create basic calculator w/ buttons
-const ref_bd= document.querySelector("body"); //get reference to body
-const cal = document.createElement("div"); // created a div where calculator would live
-cal.setAttribute("id", "calculator")
-ref_bd.appendChild(cal); //add the div into body
+const container = document.createElement("div"); // container for the calculator
+document.body.appendChild(container);//add container to body
+container.setAttribute("id", "container");
+
+const display = document.createElement("div"); //for displaying 
+const display_digits = document.createElement("p"); //the text
+display.setAttribute("id", "display");
+display_digits.textContent="0";//default start 
+
+const calcbtns = document.createElement("div"); // created a div where calculator would live
+calcbtns.setAttribute("id", "calculator");
+
+container.appendChild(display);
+display.appendChild(display_digits);
+container.appendChild(calcbtns); //add the div into body
+
+
+function createRegBtns(name){ //helper function in creating buttons
+    let curOpBtn = document.createElement("button");
+    curOpBtn.textContent= name;
+    curOpBtn.setAttribute("id", name);
+    curOpBtn.setAttribute("class","reg-btns")
+    calcbtns.appendChild(curOpBtn);
+}
 
 for(let i=0; i<10; i++){ //create buttons for 0->9
-    let cur_btn = document.createElement("button");
-    cur_btn.setAttribute("id",`${i} btn`);
-    cur_btn.textContent=i;
-    cal.appendChild(cur_btn);
+    createRegBtns(`${i}`)
 }
 
 //create buttons for operators
-const add_ref = document.createElement("button");
-add_ref.textContent= "+";
+createRegBtns("+");
+createRegBtns("-");
+createRegBtns("*");
+createRegBtns("/");
+createRegBtns(".");
+createRegBtns("=");
+createRegBtns("AC");
+createRegBtns("del");
 
-const sub_ref = document.createElement("button");
-sub_ref.textContent= "-";
-
-const div_ref = document.createElement("button");
-div_ref.textContent= "/";
-
-const mul_ref = document.createElement("button");
-mul_ref.textContent= "*";
-
-cal.appendChild(add_ref);
-cal.appendChild(sub_ref);
-cal.appendChild(mul_ref);
-cal.appendChild(div_ref);
-
-//misc buttons
-const dec_ref = document.createElement("button");
-dec_ref.textContent=".";
-const cle_ref = document.createElement("button");
-cle_ref.textContent="clear";
-const bac_ref = document.createElement("button");
-bac_ref.textContent="del";
-
-cal.appendChild(dec_ref);
-cal.appendChild(cle_ref);
-cal.appendChild(bac_ref);
+const clear_btn = document.querySelector("#AC");
+const del_btn = document.querySelector("#del");
+clear_btn.setAttribute("class", "red-btns");
+del_btn.setAttribute("class", "red-btns");// set delete and clear to diff class 
